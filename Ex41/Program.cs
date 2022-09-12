b8d7df0371610b7a7c62983c2cd28fd590192c64
Dmitrii <dtiuterev@rambler.ru>
2022-09-12 19:02:04 +0300

    Added Ex41

---
+++ Ex41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41. Подсчет количества положительных чисел, вводимых с клавиатуры. Общее количество чисел задает пользователь.
+Console.WriteLine("Вы введёте общее количество чисел и каждое из них, а я определю, сколько положительных чисел Вы ввели");
+Console.Write("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arriveNumbers = new int[m];
+
+void FillAndCalculate(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1}-е число: ");
+    arriveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+int Calculate(int[] arriveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < arriveNumbers.Length; i++)
+  {
+    if(arriveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+FillAndCalculate(m);
+Console.WriteLine($"Количество введенных положительных чисел: {Calculate(arriveNumbers)} ");
+
